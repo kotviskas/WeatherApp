61d7c138da2570637f62a1fb892a0a27381f55f4
Babak Viktoria <47895011+kotviskas@users.noreply.github.com>
2021-10-29 04:43:05 +0700

    Fixed #2

--- app/src/main/java/com/example/weatherapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/weatherapp/di/ViewModelModule.kt
@@ -10,6 +10,6 @@ val viewModelModule = module {
         MapsViewModel()
     }
     viewModel {
-        CityViewModel(get(), get())
+        CityViewModel(get())
     }
 }
--- app/src/main/java/com/example/weatherapp/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/example/weatherapp/presentation/base/BaseViewModel.kt
@@ -13,32 +13,4 @@ open class BaseViewModel : ViewModel() {
 
     protected var _apiError = SingleLiveEvent<String>()
     var apiError: LiveData<String> = _apiError
-
-    fun hasInternetConnection(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val capabilities =
-                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (capabilities != null) {
-                when {
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                        return true
-                    }
-                }
-            }
-        } else {
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-                return true
-            }
-        }
-        return false
-    }
 }
